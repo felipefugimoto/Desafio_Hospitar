9e4b59e8ac6886e90f09297238ddad241e857815
Felipe Fugimoto <30874289+felipefugimoto@users.noreply.github.com>
2024-10-10 19:34:39 -0300

    Baixado o VueJs 3 e montado o Layout e o upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Sidebar from './components/Sidebar.vue';
+
+import Footer from './components/Footer.vue';
+
+
+
+</script>
+
+<template>
+
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <Sidebar />
+    <!-- End of Sidebar -->
+
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <RouterView />
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <Footer />
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+
+
+
+
+</template>
+
+<style>
+@import url(/src/assets/vendor/fontawesome-free/css/all.min.css);
+@import url(/src/assets/css/sb-admin-2.min.css);
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import MainContent from '@/components/MainContent.vue';
+
+</script>
+
+<template>
+    <MainContent/>
+</template>
